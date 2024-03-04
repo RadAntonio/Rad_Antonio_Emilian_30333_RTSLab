840e44b451aec22efaf489f3eb2b87750d64afde
RadAntonio <tonirad@icloud.com>
2024-03-04 17:07:06 +0200

    ex3 and modiefied 1 and 2

--- RTSLabs/src/LabSession1/ex2/MatrixOperations.java
+++ RTSLabs/src/LabSession1/ex2/MatrixOperations.java
@@ -8,9 +8,19 @@ public class MatrixOperations {
 
         //printing matrix1 and matrix2 using a custom function made by me
         System.out.println("Matrix 1:");
-        printMatrix(matrix1);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix1[i][j] + " ");
+            }
+            System.out.println();
+        }
         System.out.println("\nMatrix 2:");
-        printMatrix(matrix2);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrix2[i][j] + " ");
+            }
+            System.out.println();
+        }
 
         //declaring the matrices for addition and multiplication and using custom fucntions to calculte them
         int[][] sum = addMatrices(matrix1, matrix2);
@@ -18,39 +28,40 @@ public class MatrixOperations {
 
         //printing the results
         System.out.println("\nSum of the matrices:");
-        printMatrix(sum);
-        System.out.println("\nProduct of the matrices:");
-        printMatrix(product);
-    }
-    //function for matrix addition
-    public static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
-        int[][] sum = new int[3][3];
-        for (int i = 0; i < 3; i++) {   //going through the matrices
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                sum[i][j] = matrix1[i][j] + matrix2[i][j]; //adding the elements position by position
+                System.out.print(sum[i][j] + " ");
             }
+            System.out.println();
         }
-        return sum;
+        System.out.println("\nProduct of the matrices:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(product[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
-
+    //fucntion for the multiplication of the matrices
     public static int[][] multiplyMatrices(int[][] matrix1, int[][] matrix2) {
         int[][] product = new int[3][3];
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
+            for (int j = 0; j < 3; j++) { //going through the matrices
+                for (int k = 0; k < 3; k++) { //for to calculate the sum between the product of the elements of the current line and column of the matrix
                     product[i][j] += matrix1[i][k] * matrix2[k][j];
                 }
             }
         }
         return product;
     }
-
-    public static void printMatrix(int[][] matrix) {
-        for (int i = 0; i < 3; i++) {
+    //function for matrix addition
+    public static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
+        int[][] sum = new int[3][3];
+        for (int i = 0; i < 3; i++) {   //going through the matrices using two for
             for (int j = 0; j < 3; j++) {
-                System.out.print(matrix[i][j] + " ");
+                sum[i][j] = matrix1[i][j] + matrix2[i][j]; //adding the elements position by position
             }
-            System.out.println();
         }
+        return sum;
     }
 }
